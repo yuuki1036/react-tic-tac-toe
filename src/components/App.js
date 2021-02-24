6160630893f98b89a71483a186733b9cf5d9e983
yuuki1036 <yuuki1036@gmail.com>
2021-02-24 22:36:32 +0900

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Board from './Board';
+import './App.css';
+
+function App() {
+  return (
+    <div className="game">
+      <Board />
+    </div>
+  );
+}
+
+export default App;
